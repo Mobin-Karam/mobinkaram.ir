ca36217387899e5a670caac8cc2ae61944146b51
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-05 17:56:47 +0330

    fix RootLayout

--- v3.0.4/client/app/layout.tsx
+++ v3.0.4/client/app/layout.tsx
@@ -8,7 +8,7 @@ import Home from "./page";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mobin Karam",
+  title: "Mobin Karam | Home",
   description: "Personal and Portfolio website of MobinKaram",
 };
 
@@ -19,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="lg:flex  lg:items-center lg:justify-center lg:bg-slate-400 transition-all duration-500">
+          <div className="lg:grid lg:grid-cols-5 flex flex-col-reverse w-full transition-all duration-600">
+            <div className="lg:col-start-1  lg:col-end-2 ">
+              <SideNav />
+            </div>
+            <div className="lg:col-start-2 lg:col-end-6 ">{children}</div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
