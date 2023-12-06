4d48f2384129c0b85ee5a103c4443602ebe18543
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-06 20:24:29 +0330

    fix classes

--- v3.0.4/client/app/page.tsx
+++ v3.0.4/client/app/page.tsx
@@ -5,21 +5,21 @@ import Button from "./ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-slate-500 flex items-center justify-evenly flex-col flex-nowrap">
+    <div className="w-full min-h-screen bg-slate-500 flex items-center justify-evenly flex-col flex-nowrap">
       <div className=" flex items-center justify-center flex-col flex-nowrap">
         <div className=" flex items-center justify-evenly flex-col flex-nowrap">
-          <span className="text-white font-popp text-3xl font-medium mb-40px">
+          <span className="text-white font-popp text-3xl font-medium mb-[40px]">
             Welcome
           </span>
           <span className="text-white font-popp text-6xl font-semibold">
             I&apos;m Full Stack Developer.
           </span>
-          <span className="text-white font-popp text-xl font-thin mt-40px">
+          <span className="text-white font-popp text-xl font-thin mt-[40px] mb-[40px]">
             bases in IRAN , Kermanshah, Paveh.
           </span>
           <span className=""></span>
           <Link href={"/contact"}>
-            <Button title="Hire Me" />
+            <Button title="Hire Me" className={"px-12 py-4 bg-slate-600"} />
           </Link>
         </div>
       </div>
