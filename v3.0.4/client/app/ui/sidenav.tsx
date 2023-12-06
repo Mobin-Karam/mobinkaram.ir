bd4b256df3d0e8a72dd3e22132290a6ec4482716
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-06 20:24:59 +0330

    fix button in sidenav

--- v3.0.4/client/app/ui/sidenav.tsx
+++ v3.0.4/client/app/ui/sidenav.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import Logo from "@/public/logo.png";
 import { FaBeer, FaLinkedin, FaHome } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
+import Button from "./button";
 
 export default function SideNav() {
   return (
-    <div className="lg:h-screen lg:p-[26px] p-[10px] fixed w-full bottom-0 lg:relative bg-slate-900 flex item-center justify-between lg:flex-col flex-nowrap">
+    <div className="w-full lg:min-h-screen px-4 pt-4 pb-2 fixed  bottom-0 lg:relative bg-slate-900 flex item-center justify-between lg:flex-col flex-nowrap">
       <div className="">
         <div className="w-full flex item-center justify-center flex-row flex-nowrap">
           <Link href={"/"}>
@@ -24,65 +25,39 @@ export default function SideNav() {
         </div>
       </div>
       <div className="w-full flex item-center justify-center lg:flex-col flex-row flex-nowrap">
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/"}>
-            <FaHome className="lg:hidden text-gray-200 hover:text-cyan-500 text-3xl" />
-            <span className="hidden font-popp lg:inline-block text-gray-200 hover:text-cyan-500 text-xl">
-              Home
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/aboutme"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              Aboutme
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/whatido"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              What I DO
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/resume"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              Resume
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/portfolio"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              Portfolio
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/testimonial"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              Testimonial
-            </span>
-          </Link>
-        </div>
-        <div className="w-full flex item-center justify-center flex-row flex-nowrap lg:mt-[20px]">
-          <Link href={"/contact"}>
-            <span className="text-gray-200 font-popp hover:text-cyan-500 text-xl">
-              Contact
-            </span>
-          </Link>
-        </div>
+        <Link className="" href="/">
+          <Button title="Home" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/aboutme">
+          <Button title="About Me" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/whatido">
+          <Button title="What I Do" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/resume">
+          <Button title="Resume" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/portfolio">
+          <Button title="Portfolio" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/testimonial">
+          <Button title="Testimonial" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
+        <Link className="" href="/contact">
+          <Button title="Contact" className={"w-full p-1 mt-3 m-0"} />
+        </Link>
       </div>
-      <div className="hidden lg:flex w-full items-center justify-evenly flex-row flex-nowrap ">
+      <div className="hidden lg:flex w-full items-center justify-evenly flex-row flex-nowrap mb-5 ">
         <Link href={"https://www.buymeacoffee.com/mohammadmk"} target="_blank">
           <FaBeer className="text-white w-[30px] h-[30px]" />
         </Link>
         <Link href={"https://github.com/Mobin-Karam"} target="_blank">
           <AiFillGithub className="text-white w-[30px] h-[30px]" />
         </Link>
-        <Link href={"https://www.buymeacoffee.com/mohammadmk"} target="_blank">
+        <Link
+          href={"https://linkedin.com/in/mobin-karam-a54114242"}
+          target="_blank"
+        >
           <FaLinkedin className="text-white w-[30px] h-[30px]" />
         </Link>
       </div>
