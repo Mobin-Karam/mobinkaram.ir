609116c73d2f040f6bc431b5c0da386b1a08b495
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-06 20:24:07 +0330

    add conditional code to button

--- v3.0.4/client/app/ui/button.tsx
+++ v3.0.4/client/app/ui/button.tsx
@@ -1,12 +1,21 @@
 // Button
 
+import clsx from "clsx";
+
 type ButtonProps = {
   title: string;
+  className?: any;
 };
 
-export default function Button({ title }: ButtonProps) {
+export default function Button({ title, className, ...rest }: ButtonProps) {
   return (
-    <button className="text-sky-500 border-sky-500 p-[20px] border-2 bg-sky-900/20 px-[40px] hover:bg-sky-500 hover:text-white font-bold mt-[50px] rounded-full transition-all duration-400">
+    <button
+      {...rest}
+      className={clsx(
+        "text-sky-500 border-sky-500 border-2 bg-sky-900/20 hover:bg-sky-700 hover:text-white font-bold rounded-full transition-all duration-400",
+        className
+      )}
+    >
       {title}
     </button>
   );
