4b6d6414adfc88f973c5437e507c48058dbf3ef4
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-12-08 00:04:06 +0330

    make overflow for all layout

--- v3.0.4/client/app/layout.tsx
+++ v3.0.4/client/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
             <div className="lg:col-start-1  lg:col-end-2 ">
               <SideNav />
             </div>
-            <div className="w-full h-screen lg:col-start-2 lg:col-end-6">
+            <div className="w-full h-screen lg:col-start-2 lg:col-end-6 overflow-auto">
               {children}
             </div>
           </div>
