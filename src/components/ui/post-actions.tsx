a85c9d081310babbc8ac46ad8adfb3da40368aec
mobinkaram <mohammadmobinkaram@gmail.com>
2026-02-15 13:46:56 +0330

    fix: remove duplicate escapeHtml/slugify in post actions

--- src/components/ui/post-actions.tsx
+++ src/components/ui/post-actions.tsx
@@ -146,18 +146,3 @@ function ActionButton({
     </button>
   );
 }
-
-function slugify(str: string) {
-  return str
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/^-+|-+$/g, "")
-    .slice(0, 60);
-}
-
-function escapeHtml(str: string) {
-  return str
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
-}
